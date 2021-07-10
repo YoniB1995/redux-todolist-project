b7774f4bd91d8eff1d9ebdd3982e0be550796cd7
Yoni <yonatansamfisher@gmail.com>
2021-07-10 19:14:00 +0300

    added items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import './App.css';
+import Homepage from './Components/Pages/Homepage';
+import styled from 'styled-components';
+
+const AppBody = styled.div` 
+margin:0;
+padding:0;
+box-sizing:border-box;
+background-image: url("images/backgroundImg.jpeg");
+background-size: cover;
+background-attachment: fixed;
+
+`
 
 function App() {
   return (
-    <div className="App">
-      
-       
-      
-        
-    </div>
+    <AppBody>
+      <Homepage/>
+    </AppBody>
   );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import todoReducer from '../Components/Features/todoSlice'
 export const store = configureStore({
   reducer: {
-   
+   todos: todoReducer
   },
 });
