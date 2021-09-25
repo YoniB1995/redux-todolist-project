af2fb7d4d6d0f5b53025e779b6b2426b077d3b91
YoniB1995 <yonatansamfisher@gmail.com>
2021-09-25 18:58:47 +0300

    basic testing

--- src/Components/Features/Counter/Counter.jsx
+++ src/Components/Features/Counter/Counter.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
+  const [num, setNum] = useState(0);
+
+  const incrementNum = () => {
+    setNum(num + 1);
+  };
+  const decrementNum = () => {
+    setNum(num - 1);
+  };
   return (
     <div>
       <h1 data-testid="header">My Counter</h1>
+      <h2 data-testid="counter">{num}</h2>
+      <button onClick={incrementNum} data-testid="add-btn">
+        +
+      </button>
+      <button onClick={decrementNum} data-testid="add-subtract">
+        -
+      </button>
     </div>
   );
 };
